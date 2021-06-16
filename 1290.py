7b5cd4b97410796a3fd27a21282374a449ff30fb
Ramil Mavlyutov <63664630+stirk1337@users.noreply.github.com>
2021-06-17 01:45:32 +0500

    Create 1290.py

---
+++ 1290.py
@@ -0,0 +1,37 @@
+def merge(alist):
+    if len(alist)>1:
+        mid = len(alist)//2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+        merge(lefthalf)
+        merge(righthalf)
+        i=0
+        j=0
+        k=0
+        while i<len(lefthalf) and j<len(righthalf):
+            if lefthalf[i]<righthalf[j]:
+                alist[k]=lefthalf[i]
+                i=i+1
+            else:
+                alist[k]=righthalf[j]
+                j=j+1
+            k=k+1
+        while i<len(lefthalf):
+            alist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+        while j<len(righthalf):
+            alist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+    return alist
+
+
+n = int(input())
+lst = list()
+for i in range(n):
+	lst.append(int(input()))
+
+lst = merge(lst)
+lst.reverse()
+for i in lst: print(i)
