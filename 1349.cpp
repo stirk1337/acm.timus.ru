91347553d9afaf74f05abd34c6abb4d5212b182b
Ramil Mavlyutov <63664630+stirk1337@users.noreply.github.com>
2021-06-19 22:00:42 +0500

    Create 1349.cpp

---
+++ 1349.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a;
+	cin >> a;
+	if (a == 1)
+	{
+		cout << 1 << " " << 2 << " " << 3;
+	}
+	else if (a == 2)
+	{
+		cout << 3 << " " << 4 << " " << 5;
+	}
+	else
+	{
+		cout << -1;
+	}
+	return 0;
+}
