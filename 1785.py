803baf33e3d2299e1a61f9c60f7f0cc8b7218385
Ramil Mavlyutov <63664630+stirk1337@users.noreply.github.com>
2021-07-04 23:14:14 +0500

    Create 1785.py

---
+++ 1785.py
@@ -0,0 +1,19 @@
+n = int(input())
+if(n>=1000):
+    print("legion")
+elif (n>=500):
+    print("zounds")
+elif (n>=250):
+    print("swarm")
+elif (n>=100):
+    print("throng")
+elif (n>=50):
+    print("horde")
+elif (n>=20):
+    print("lots")
+elif (n>=10):
+    print("pack")
+elif (n>=5):
+    print("several")
+else:
+    print("few")
