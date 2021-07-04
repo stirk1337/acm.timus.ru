6a53a6569607a3b5459e5ec94c0a8ddda3cf264d
Ramil Mavlyutov <63664630+stirk1337@users.noreply.github.com>
2021-07-04 23:13:34 +0500

    Create 1731.py

---
+++ 1731.py
@@ -0,0 +1,7 @@
+from random import randint
+n, m = map(int, input().split())
+for i in range(n):
+	print(randint(1,1000000001), end = " ")
+print()
+for i in range(m):
+	print(randint(1,1000000001), end = " ")
