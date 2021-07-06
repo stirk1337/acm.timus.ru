d9eb13b04f81dd51adeb5c92058e416be30fddff
Ramil Mavlyutov <63664630+stirk1337@users.noreply.github.com>
2021-07-06 18:00:11 +0500

    Create 1873.py

---
+++ 1873.py
@@ -0,0 +1,31 @@
+n = int(input())
+
+if n == 0:
+	print(5)
+elif n == 1:
+	print(21)
+if n == 2:
+	print(12)
+elif n == 3:
+	print(2)
+if n == 4:
+	print(1)
+elif n == 5:
+	print(4)
+if n == 6:
+	print(6)
+elif n == 7:
+	print(1)
+if n == 8:
+	print(4)
+elif n == 9:
+	print(4)
+if n == 10:
+	print(1)
+elif n == 11:
+	print(0)
+if n == 12:
+	print(1)
+elif n == 13:
+	print(1)
+
